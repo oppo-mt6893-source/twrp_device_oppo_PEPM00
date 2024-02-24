2202d46acfd223cb4b08501a7a0d42101e6b8ae7
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-24 15:35:49 +0800

    PEPM00: fix flags

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/oppo/pepm00
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31 #
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true # 动态分区# Soong namespaces
+PRODUCT_SOONG_NAMESPACES +=$(DEVICE_PATH)#
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_pepm00.mk
 
